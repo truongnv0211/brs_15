21039091659db56457a58727a42b087839a2a9e6
truongnv0211 <nguyen.van.truong@framgia.com>
2015-07-09 15:00:04 +0700

    Fix no save username, improve header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def after_sign_in_path_for resource
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
   def required_admin
     authenticate_user!
     redirect_to root_path unless current_user.admin?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) {|u| u.permit :signin}
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit :email,
+      :name, :password, :password_confirmation}
   end
 end
